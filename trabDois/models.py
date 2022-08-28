20e982d150af4894c6252d37f7fc437ff1ad4c95
FelipeeBR <felipemen74@gmail.com>
2022-08-28 20:09:53 -0300

    Primeiro commit

---
+++ trabDois/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField('Nome',
+    max_length=50)
+    sobrenome = models.CharField('Sobrenome',
+    max_length=100)
+    periodo_ingresso = models.CharField('Período de Ingresso',max_length=50)
+    nota = models.DecimalField('Nota - Web Avançado', decimal_places=2,
+    max_digits=4)
+    situacao = models.CharField('Situacao', max_length=50)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
---
+++ trabDois/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from trabDois.models import Aluno
+
+# Create your views here.
+
+def index(request):
+    alunos = Aluno.objects.all()
+    testChave = {
+        'curso': 'Desenvolvimento Web Avançado',
+        'alunos': alunos
+    }
+    return render(request,'index.html', testChave)
+
+def aluno(request, id):
+    aluno = Aluno.objects.get(id=id)
+    context = {
+
+        'aluno': aluno
+
+    }
+    return render(request, 'aluno.html', context)
